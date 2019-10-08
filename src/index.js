8cb02f929ac7a08b43a027cac9889ebc6301a7a3
Kevin Hyer <kevin.hyer@live.com>
2019-10-08 02:19:47 -0400

    Finish reverting preact

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { render } from "preact";
 
-// root holds our app's root DOM Element:
-let root;
-
-function init() {
-  root = render(<App />, document.body, root);
-}
-init();
-
-// example: Re-render on Webpack HMR update:
-if (module.hot) module.hot.accept("./App", init);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
